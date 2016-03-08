5490b55ec134587b9c677c740941301197929e8b
xiaodaxia1083394272 <1083394272@qq.com>
2016-03-08 14:05:30 +0800

    完善

---
+++ 格式转换.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+    url:'/user/userlog?page='+click_page,
+
+
+                 var history_string = history_content_height+"px";
+                 var main_string = main_div_height+"px";    //拼的时候自动转字符串
+
+
+function timeStampToDate(timeStampString) {
+  return new Date(parseInt(timeStampString) * 1000);   //转换时间格式  
+}
+
+
+
+
+
+
+
+
+
